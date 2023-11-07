8fb3aa207214279deb566cac5dfc4a470c09a742
Maya Shah <mayashah.tam@tamu.edu>
2023-11-07 12:54:43 -0600

    added buttons for add-ons and fixed issues with size buttons not update from previous correction

--- webapp/functions/functions.js
+++ webapp/functions/functions.js
@@ -20,6 +20,14 @@ function buildDrink() {
 }
 
 function getDrinkSize() {
+    const addOns1 = document.getElementById("addOns1");
+    if (addOns1.style.display == "block") {
+        addOns1.style.display = "none";
+    }
+    const addOns2 = document.getElementById("addOns1");
+    if (addOns2.style.display == "block") {
+        addOns2.style.display = "none";
+    }
     const size = document.getElementById("size");
     size.style.display = "block";
 }
@@ -29,13 +37,37 @@ function getDrinkReplacements() {
 }
 
 function getDrinkAddOn1() {
+    const size = document.getElementById("size");
+    if (size.style.display == "block") {
+        size.style.display = "none";
+    }
+    const addOns2 = document.getElementById("addOns1");
+    if (addOns2.style.display == "block") {
+        addOns2.style.display = "none";
+    }
+    const addOns1 = document.getElementById("addOns1");
+    addOns1.style.display = "block";
 
 }
 
 function getDrinkAddOn2() {
-
+    const size = document.getElementById("size");
+    if (size.style.display == "block") {
+        size.style.display = "none";
+    }
+    const addOns1 = document.getElementById("addOns1");
+    if (addOns1.style.display == "block") {
+        addOns1.style.display = "none";
+    }
+    const addOns2 = document.getElementById("addOns2");
+    addOns2.style.display = "block";
 }
 
 function getDrinkQuantity() {
 
+}
+
+function backCustomerHome() {
+    var currLocation = window.location.href;
+    location.href = currLocation.replace("build_drink", "customer_home");
 }
