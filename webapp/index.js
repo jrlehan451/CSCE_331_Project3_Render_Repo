aba057f74e4af64b090dc0572cc4aeb9442598ca
Gabe Wild <wildmangabe@gmail.com>
2023-11-07 22:29:34 -0600

    Revert "Merge pull request #2 from csce-315-331-2023c/MaxBranchManager"
    This reverts commit 6c3d8f7c468f43e6aa8401da1f31e3ed349e7850, reversing
    changes made to 4cbdd39706517b5c4acadafd62f4b421fabe37b5.

--- webapp/index.js
+++ webapp/index.js
@@ -1,146 +1,235 @@
-const express = require('express');
-const { Pool } = require('pg');
-const dotenv = require('dotenv').config();
+const express = require("express");
+const { Pool } = require("pg");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
 
 // Create express app
 const app = express();
-const port = 3000;
+const port = 4000;
 
-app.use('/css', express.static('css'));
-app.use('/js', express.static('functions'));
+app.use("/css", express.static("css"));
+app.use("/js", express.static("functions"));
+app.use(express.static("images"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 
-app.locals.capitalizeName = function(name, delimiter) {
-    const words = name.split(delimiter);
+app.locals.capitalizeName = function (name, delimiter) {
+  const words = name.split(delimiter);
 
-    for (let i = 0; i < words.length; i++) {
-        words[i] = words[i][0].toUpperCase() + words[i].substr(1);
-    }
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+  }
 
-    return words.join(' ');
-}
+  return words.join(" ");
+};
 
 // Create pool
 const pool = new Pool({
-    user: process.env.PSQL_USER,
-    host: process.env.PSQL_HOST,
-    database: process.env.PSQL_DATABASE,
-    password: process.env.PSQL_PASSWORD,
-    port: process.env.PSQL_PORT,
-    ssl: {rejectUnauthorized: false}
+  user: process.env.PSQL_USER,
+  host: process.env.PSQL_HOST,
+  database: process.env.PSQL_DATABASE,
+  password: process.env.PSQL_PASSWORD,
+  port: process.env.PSQL_PORT,
+  ssl: { rejectUnauthorized: false },
 });
 
 // Add process hook to shutdown pool
-process.on('SIGINT', function() {
-    pool.end();
-    console.log('Application successfully shutdown');
-    process.exit(0);
+process.on("SIGINT", function () {
+  pool.end();
+  console.log("Application successfully shutdown");
+  process.exit(0);
 });
 
 app.set("view engine", "ejs");
 
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', (req, res) => {
-    const data = {name: 'Mario'};
-    res.render('index', data);
-});
-
-app.get('/user', (req, res) => {
-    teammembers = []
+    employees = []
     pool
-        .query('SELECT * FROM teammembers;')
+        .query('SELECT * FROM employees;')
         .then(query_res => {
             for (let i = 0; i < query_res.rowCount; i++){
-                teammembers.push(query_res.rows[i]);
+                employees.push(query_res.rows[i]);
             }
-            const data = {teammembers: teammembers};
-            console.log(teammembers);
-            res.render('user', data);        
+            const data = {employees: employees};
+            console.log(employees);
+            res.render('index', data);        
         });
 });
 
-app.get('/drink_options', (req, res) => {
-    drink_categories = []
-    pool
-        .query('SELECT DISTINCT category FROM drinks;')
-        .then(query_res => {
-            for (let i = 0; i < query_res.rowCount; i++){
-                drink_categories.push(query_res.rows[i]);
-            }
-            const data = {drink_categories: drink_categories};
-            console.log(drink_categories);
-            res.render('drink_options', data);        
-        }); 
-});
-
-app.get('/add_drink', (req, res) => {
-    drinks = []
-    category = req.query.category;
-    const query = {
-        text: 'SELECT * FROM drinks WHERE category = $1',
-        values: [category],
-    }
-    pool
-        .query(query)
-        .then(query_res => {
-            for (let i = 0; i < query_res.rowCount; i++){
-                drinks.push(query_res.rows[i]);
-            }                
-            const data = {drinks: drinks};
-            console.log(drinks);
-            res.render('add_drink', data);        
-        });
-});
-
-app.get('/customer_home', (req, res) => {
-    drink_categories = []
-    pool
-        .query('SELECT DISTINCT category FROM drinks;')
-        .then(query_res => {
-            for (let i = 0; i < query_res.rowCount; i++){
-                drink_categories.push(query_res.rows[i]);
-            }
-            const data = {drink_categories: drink_categories};
-            console.log(drink_categories);
-            res.render('customer_home', data);        
-        }); 
-});
-
-app.get('/manager', (req, res) => {
+app.get('/manager_main', (req, res) => {
     const data = {name: 'Mario'};
     res.render('manager_main', data);
 });
 
-// app.get('/inventory', (req, res) => {
-//     const data = {name: 'Mario'};
-//     res.render('inventory', data);
-// });
 
-// Add this route to fetch the inventory data
-app.get('/inventory', (req, res) => {
-    const query = 'SELECT * FROM inventory_items ORDER BY name';
-    pool.query(query)
+app.post("/post_name", cors(), async (req, res) => {
+  let { name } = req.body;
+  console.log(name);
+});
+
+app.get("/home", cors(), async (req, res) => {
+  res.send("Connection between the webapp and frontend");
+});
+
+app.get("/user", (req, res) => {
+  teammembers = [];
+  pool.query("SELECT * FROM teammembers;").then((query_res) => {
+    for (let i = 0; i < query_res.rowCount; i++) {
+      teammembers.push(query_res.rows[i]);
+    }
+    const data = { teammembers: teammembers };
+    console.log(teammembers);
+    res.render("user", data);
+  });
+});
+
+app.get("/drink_options", (req, res) => {
+  drink_categories = [];
+  pool.query("SELECT DISTINCT category FROM drinks;").then((query_res) => {
+    for (let i = 0; i < query_res.rowCount; i++) {
+      drink_categories.push(query_res.rows[i]);
+    }
+    const data = { drink_categories: drink_categories };
+    console.log(drink_categories);
+    res.render("drink_options", data);
+  });
+});
+
+app.get("/add_drink", (req, res) => {
+  drinks = [];
+  category = req.query.category;
+  const query = {
+    text: "SELECT * FROM drinks WHERE category = $1",
+    values: [category],
+  };
+  pool.query(query).then((query_res) => {
+    for (let i = 0; i < query_res.rowCount; i++) {
+      drinks.push(query_res.rows[i]);
+    }
+    const data = { drinks: drinks };
+    console.log(drinks);
+    res.render("add_drink", data);
+  });
+});
+
+app.get("/add_on", (req, res) => {
+  add_ons = [];
+  pool.query("SELECT * FROM ingredients WHERE cost > 0;").then((query_res) => {
+    for (let i = 0; i < query_res.rowCount; i++) {
+      add_ons.push(query_res.rows[i]);
+    }
+    const data = { add_ons: add_ons };
+    console.log(add_ons);
+    res.render("add_on", data);
+  });
+});
+
+app.get("/customer_home", (req, res) => {
+  drink_categories = [];
+  pool.query("SELECT DISTINCT category FROM drinks;").then((query_res) => {
+    for (let i = 0; i < query_res.rowCount; i++) {
+      drink_categories.push(query_res.rows[i]);
+    }
+    const data = { drink_categories: drink_categories };
+    console.log(drink_categories);
+    res.render("customer_home", data);
+  });
+});
+
+app.get("/new_order", (req, res) => {
+  res.render("new_order");
+});
+
+app.get("/order_summary", (req, res) => {
+  res.render("order_summary");
+});
+
+app.get("/drink_series", (req, res) => {
+  drink_categories = [];
+  pool.query("SELECT DISTINCT category FROM drinks;").then((query_res) => {
+    for (let i = 0; i < query_res.rowCount; i++) {
+      drink_categories.push(query_res.rows[i]);
+    }
+    console.log(drink_categories);
+  });
+  drinks = [];
+  category = req.query.category;
+  const query = {
+    text: "SELECT * FROM drinks WHERE category = $1",
+    values: [category],
+  };
+  pool.query(query).then((query_res) => {
+    for (let i = 0; i < query_res.rowCount; i++) {
+      drinks.push(query_res.rows[i]);
+    }
+    console.log(drinks);
+    res.render("drink_series", {
+      drink_categories: drink_categories,
+      drinks: drinks,
+    });
+  });
+});
+
+app.get("/build_drink", (req, res) => {
+  add_ons = [];
+  pool.query("SELECT * FROM ingredients WHERE cost > 0;").then((query_res) => {
+    for (let i = 0; i < query_res.rowCount; i++) {
+      add_ons.push(query_res.rows[i]);
+    }
+    const data = { add_ons: add_ons };
+    console.log(add_ons);
+    res.render("build_drink", data);
+  });
+});
+
+app.get("/view_cart", (req, res) => {
+  res.render("view_cart")
+});
+
+app.get("/customer_checkout", (req, res) => {
+  res.render("customer_checkout")
+});
+
+app.get('/menu', (req, res)=>{
+    const drinksByCategory = {};
+
+    pool
+        .query('SELECT * FROM drinks;')
         .then(query_res => {
-            const inventoryData = query_res.rows;
-            console.log(inventoryData);
-            res.render('inventory', { inventoryData });
+            const drinks = query_res.rows;
+
+            // Organize drinks by category
+            drinks.forEach(drink => {
+                const category = drink.category;
+
+                if (!drinksByCategory[category]) {
+                    drinksByCategory[category] = [];
+                }
+
+                drinksByCategory[category].push(drink);
+            });
+
+            res.render('menu', {drinksByCategory});
+        })
+})
+
+app.get('/menu-addons', (req, res) => {
+    addOns = []; 
+
+    pool.query('SELECT * FROM add_ons;')
+        .then(query_res => {
+            for (let i = 0; i < query_res.rowCount; i++) {
+                addOns.push(query_res.rows[i]);
+            }
+
+            const data = { addOns: addOns };
+            res.render('menu-addons', data);
         })
 });
 
-app.get('/menu_items', (req, res) => {
-    const drinksQuery = 'SELECT * FROM drinks ORDER BY name';
-    const ingredientsQuery = 'SELECT * FROM ingredients ORDER BY name';
-
-    Promise.all([
-        pool.query(drinksQuery),
-        pool.query(ingredientsQuery)
-    ])
-    .then(([drinksRes, ingredientsRes]) => {
-        const menuData = drinksRes.rows;
-        const ingredientsData = ingredientsRes.rows;
-        console.log(menuData, ingredientsData);
-        res.render('menu_items', { menuData, ingredientsData });
-    })
-});
-
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
