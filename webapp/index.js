e6c55ce4ec07abb47613802b9a78d8e0391ca855
dutyMax <94495083+dutyMax@users.noreply.github.com>
2023-11-07 22:59:37 -0600

    Fixed issues with existing functionallity

--- webapp/index.js
+++ webapp/index.js
@@ -303,6 +303,32 @@ app.get('/menu-addons', (req, res) => {
         })
 });
 
+app.get('/inventory', (req, res) => {
+  const query = 'SELECT * FROM inventory_items ORDER BY name';
+  pool.query(query)
+      .then(query_res => {
+          const inventoryData = query_res.rows;
+          console.log(inventoryData);
+          res.render('inventory', { inventoryData });
+      })
+});
+
+app.get('/menu_items', (req, res) => {
+  const drinksQuery = 'SELECT * FROM drinks ORDER BY name';
+  const ingredientsQuery = 'SELECT * FROM ingredients ORDER BY name';
+
+  Promise.all([
+      pool.query(drinksQuery),
+      pool.query(ingredientsQuery)
+  ])
+  .then(([drinksRes, ingredientsRes]) => {
+      const menuData = drinksRes.rows;
+      const ingredientsData = ingredientsRes.rows;
+      console.log(menuData, ingredientsData);
+      res.render('menu_items', { menuData, ingredientsData });
+  })
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
