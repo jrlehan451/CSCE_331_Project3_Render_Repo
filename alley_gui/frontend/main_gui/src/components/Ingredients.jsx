d6f4643e3176d4ee3e6594fd54f2f0a3e726d0c0
Vasudev08 <vasu14devagarwal@gmail.com>
2023-11-19 18:41:23 -0600

    added 3 input fields with typesafety

--- alley_gui/frontend/main_gui/src/components/Ingredients.jsx
+++ alley_gui/frontend/main_gui/src/components/Ingredients.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "../theme";
+import { TextField, InputLabel, FormControl } from "@mui/material";
 
 const Ingredients = () => {
   // Creating columns for displaying sql queries
@@ -16,12 +17,30 @@ const Ingredients = () => {
 
   const [values, setValues] = useState({
     ingredientId: "",
-    inventoryId: "",
     name: "",
     cost: "",
   });
 
+  const [inputErrors, setInputErrors] = useState({
+    ingredientId: false,
+    name: false,
+    amount: false,
+    quantityPerUnit: false,
+  });
+
   const [data, setData] = useState([]);
+
+  const handleNumberInputChange = (e, key) => {
+    // Allow only valid integers in the input
+    const newValue = parseInt(e.target.value, 10);
+
+    if (!isNaN(newValue)) {
+      setValues({ ...values, [key]: newValue });
+      setInputErrors({ ...inputErrors, [key]: false });
+    } else {
+      setInputErrors({ ...inputErrors, [key]: true });
+    }
+  };
 
   // Getting inventory from the backend
   useEffect(() => {
@@ -65,6 +84,47 @@ const Ingredients = () => {
 
       <div style={{ height: 400, width: "80vw", marginBottom: "20px" }}>
         <DataGrid rows={data} columns={columns} columnBuffer={2} />
+      </div>
+
+      <div>
+        <InputLabel htmlFor="filled-basic">Ingredient ID</InputLabel>
+        <FormControl>
+          <TextField
+            id="filled-basic"
+            variant="filled"
+            onChange={(e) => handleNumberInputChange(e, "itemId")}
+            value={values.ingredientId}
+            type="number"
+            error={inputErrors.ingredientId}
+            helperText={
+              inputErrors.ingredientId ? "Please enter a valid integer" : ""
+            }
+          />
+        </FormControl>
+      </div>
+      <div>
+        <InputLabel htmlFor="filled-basic">Name</InputLabel>
+        <FormControl>
+          <TextField
+            id="filled-basic"
+            variant="filled"
+            onChange={(e) => setValues({ ...values, name: e.target.value })}
+          />
+        </FormControl>
+      </div>
+      <div>
+        <InputLabel htmlFor="filled-basic">Cost</InputLabel>
+        <FormControl>
+          <TextField
+            id="filled-basic"
+            variant="filled"
+            onChange={(e) => handleNumberInputChange(e, "cost")}
+            value={values.cost}
+            type="number"
+            error={inputErrors.cost}
+            helperText={inputErrors.cost ? "Please enter a valid integer" : ""}
+          />
+        </FormControl>
       </div>
     </div>
   );
