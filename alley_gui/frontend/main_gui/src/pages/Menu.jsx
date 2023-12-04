6cc23892c4eb71091e189665dc64b631d25ec855
Vasudev08 <vasu14devagarwal@gmail.com>
2023-12-04 00:46:23 -0600

    text to speech completed for menu

--- alley_gui/frontend/main_gui/src/pages/Menu.jsx
+++ alley_gui/frontend/main_gui/src/pages/Menu.jsx
@@ -1,85 +1,123 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import HomeButton from './images/HomeButton.png';
-import arrow from './images/back_arrow.png';
+import HomeButton from "./images/HomeButton.png";
+import arrow from "./images/back_arrow.png";
 
-import './Menu.css'
+import "./Menu.css";
+import {
+  handleHover,
+  handleMouseOut,
+  handleTextFieldSpeech,
+  handleTableFieldSpeech,
+} from "../components/SpeechUtils";
+import TextToSpeech from "../components/TextToSpeech";
 
 const Menu = () => {
-    const [drinks, setDrinks] = useState([]);
-
-    function capitalizeName(name, delimiter) {
-        const words = name.split(delimiter);
-      
-        for (let i = 0; i < words.length; i++) {
-          words[i] = words[i][0].toUpperCase() + words[i].substr(1);
-        }
-      
-        return words.join(" ");
-    };
-
-    function getImage(name){
-        name = "/drink_images/" + name;
-        name += ".png"
-        const words = name.split(" ");
-        return words.join("_");
-    }
-  
-    const returnHome = () => {
-        window.location.href = "/";
-    };
-  
-    const navigateToMenuAddons = () => {
-        var currLocation = window.location.href;
-        window.location.href = currLocation.replace("Menu", "MenuAddOns");
-    };
-
-    useEffect(() => {
-        const drinksByCategory = async () => {
-        try {
-            const response = await axios.get(
-                "https://thealley.onrender.com/menu_jsx"
-            );
-            console.log(response.data);
-            console.log(response.drinksByCategory);
-            const jsonVals = await response.data;
-            console.log(jsonVals.drinksByCategory);
-            const drinks = jsonVals.data.drinksByCategory;
-            setDrinks(drinks);
-
-        } catch (err) {
-            console.log("ERROR");
-            console.error(err.message);
-        }
-        };
-
-        drinksByCategory();
-    }, []);
-
-    return (
-      <div>
-        <h1 className="menu-title">Step 1: Choose Your Drink</h1>
-        <button className="home-button" onClick={returnHome}>
-            <img src={HomeButton} alt="home" />
-        </button>
-        <button className="navigationToMenuAddOns" onClick={navigateToMenuAddons}>
-            <img src={arrow} alt="arrow" />
-        </button>
-        <div className="drink-panel">
-            {Object.keys(drinks).map(category => (
-                <div class = "category-panel" key={category}>
-                    <h2 class="category-title"> {capitalizeName(category, '_')} </h2>
-                        {drinks[category].map(drink => (
-                            <div class="drink-entry" key={drink}> 
-                                <img class ="drink-square" src={getImage(category)} alt={capitalizeName(drink.name, ' ')} onError={(e) => {e.target.src = "/drink_images/placeholder.png"}} />
-                                <p class="drink-name">{capitalizeName(drink.name, ' ')}</p> 
-                            </div>                    
-                        ))}
-                </div>
-            ))}
-        </div>
-      </div>
-    );
+  const [drinks, setDrinks] = useState([]);
+  const [isHoverEnabled, setIsHoverEnabled] = useState(false);
+  const toggleHover = () => {
+    setIsHoverEnabled((prevIsHoverEnabled) => !prevIsHoverEnabled);
   };
-  
-  export default Menu;
+  const handleGridCellHover = (params) => {
+    console.log("handleGridCellHover is called!");
+
+    if (isHoverEnabled) {
+      console.log("isHoverEnabled is false");
+
+      const cellContent = params.value.toString();
+      console.log("Cell Content:", cellContent);
+
+      // Call the handleHover function to initiate text-to-speech
+      handleTableFieldSpeech(cellContent);
+      //handleTableFieldSpeech("This is a test");
+    }
+  };
+
+  function capitalizeName(name, delimiter) {
+    const words = name.split(delimiter);
+
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+    }
+
+    return words.join(" ");
+  }
+
+  function getImage(name) {
+    name = "/drink_images/" + name;
+    name += ".png";
+    const words = name.split(" ");
+    return words.join("_");
+  }
+
+  const returnHome = () => {
+    window.location.href = "/";
+  };
+
+  const navigateToMenuAddons = () => {
+    var currLocation = window.location.href;
+    window.location.href = currLocation.replace("Menu", "MenuAddOns");
+  };
+
+  useEffect(() => {
+    const drinksByCategory = async () => {
+      try {
+        const response = await axios.get(
+          "https://thealley.onrender.com/menu_jsx"
+        );
+        console.log(response.data);
+        console.log(response.drinksByCategory);
+        const jsonVals = await response.data;
+        console.log(jsonVals.drinksByCategory);
+        const drinks = jsonVals.data.drinksByCategory;
+        setDrinks(drinks);
+      } catch (err) {
+        console.log("ERROR");
+        console.error(err.message);
+      }
+    };
+
+    drinksByCategory();
+  }, []);
+
+  return (
+    <div>
+      <h1 className="menu-title">Step 1: Choose Your Drink</h1>
+      <button className="home-button" onClick={returnHome}>
+        <img src={HomeButton} alt="home" />
+      </button>
+
+      <button className="navigationToMenuAddOns" onClick={navigateToMenuAddons}>
+        <img src={arrow} alt="arrow" />
+      </button>
+      <div className="drink-panel">
+        {Object.keys(drinks).map((category) => (
+          <div class="category-panel" key={category}>
+            <h2 class="category-title"> {capitalizeName(category, "_")} </h2>
+            {drinks[category].map((drink) => (
+              <div
+                class="drink-entry"
+                key={drink}
+                onMouseOver={(e) => handleHover(e, isHoverEnabled)}
+                onMouseOut={handleMouseOut}
+              >
+                <img
+                  class="drink-square"
+                  src={getImage(category)}
+                  alt={capitalizeName(drink.name, " ")}
+                  onError={(e) => {
+                    e.target.src = "/drink_images/placeholder.png";
+                  }}
+                />
+                <p class="drink-name">{capitalizeName(drink.name, " ")}</p>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+      <TextToSpeech isHoverEnabled={isHoverEnabled} toggleHover={toggleHover} />
+    </div>
+  );
+};
+
+export default Menu;
--- alley_gui/frontend/main_gui/src/pages/MenuAddOns.jsx
+++ alley_gui/frontend/main_gui/src/pages/MenuAddOns.jsx
@@ -1,74 +1,112 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import HomeButton from './images/HomeButton.png';
-import arrow from './images/back_arrow.png';
+import HomeButton from "./images/HomeButton.png";
+import arrow from "./images/back_arrow.png";
 
-import './MenuAddOns.css';
+import "./MenuAddOns.css";
+import {
+  handleHover,
+  handleMouseOut,
+  handleTextFieldSpeech,
+  handleTableFieldSpeech,
+} from "../components/SpeechUtils";
+import TextToSpeech from "../components/TextToSpeech";
 
 const MenuAddons = () => {
-    const [addOns, setAddOns] = useState([]);
+  const [addOns, setAddOns] = useState([]);
+  const [isHoverEnabled, setIsHoverEnabled] = useState(false);
+  const toggleHover = () => {
+    setIsHoverEnabled((prevIsHoverEnabled) => !prevIsHoverEnabled);
+  };
+  const handleGridCellHover = (params) => {
+    console.log("handleGridCellHover is called!");
 
-    function capitalizeName(name, delimiter) {
-        const words = name.split(delimiter);
-      
-        for (let i = 0; i < words.length; i++) {
-          words[i] = words[i][0].toUpperCase() + words[i].substr(1);
-        }
-      
-        return words.join(" ");
-    };
-  
-    const returnHome = () => {
-        window.location.href = "/";
-    };
-  
-    const navigateToMenu = () => {
-        var currLocation = window.location.href;
-        window.location.href = currLocation.replace("MenuAddOns", "Menu");
-    };
+    if (isHoverEnabled) {
+      console.log("isHoverEnabled is false");
 
-    function getImage(name){
-      name = "/addOn_images/" + name;
-      name += ".png"
-      const words = name.split(" ");
-      return words.join("_");
+      const cellContent = params.value.toString();
+      console.log("Cell Content:", cellContent);
+
+      // Call the handleHover function to initiate text-to-speech
+      handleTableFieldSpeech(cellContent);
+      //handleTableFieldSpeech("This is a test");
+    }
+  };
+
+  function capitalizeName(name, delimiter) {
+    const words = name.split(delimiter);
+
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+    }
+
+    return words.join(" ");
   }
 
-    useEffect(() => {
-        const getAddOns = async () => {
-          try {
-            const response = await axios.get(
-              "https://thealley.onrender.com/add_on_jsx"
-            );
-            const jsonVals = await response.data;
-            setAddOns(jsonVals.data.add_ons.rows);
-          } catch (err) {
-            console.log("ERROR");
-            console.error(err.message);
-          }
-        };
-    
-        getAddOns();
-      }, []);
-    return (
-      <div>
-        <h1 className="menu-title">Step 2: Choose Your Add-Ons (Max 2)</h1>
-        <button className="home-button" onClick={returnHome}>
-            <img src={HomeButton} alt="home" />
-        </button>
-        <button className="navigationToMenu" onClick={navigateToMenu}>
-            <img src={arrow} alr="arrow" />
-        </button>
-        <div className="addon-container">
-          {addOns.map((addon, index) => (
-            <div className="addon-entry" key={index}>
-              <img class ="addon-square" src={getImage(addon.name)} alt={capitalizeName(addon.name, ' ')} onError={(e) => {e.target.src = "/addOn_images/placeholder.png"}}/>
-              <div className="addon-name">{capitalizeName(addon.name, ' ')}</div>
-            </div>
-          ))}
-        </div>
-      </div>
-    );
+  const returnHome = () => {
+    window.location.href = "/";
   };
-  
-  export default MenuAddons;
+
+  const navigateToMenu = () => {
+    var currLocation = window.location.href;
+    window.location.href = currLocation.replace("MenuAddOns", "Menu");
+  };
+
+  function getImage(name) {
+    name = "/addOn_images/" + name;
+    name += ".png";
+    const words = name.split(" ");
+    return words.join("_");
+  }
+
+  useEffect(() => {
+    const getAddOns = async () => {
+      try {
+        const response = await axios.get(
+          "https://thealley.onrender.com/add_on_jsx"
+        );
+        const jsonVals = await response.data;
+        setAddOns(jsonVals.data.add_ons.rows);
+      } catch (err) {
+        console.log("ERROR");
+        console.error(err.message);
+      }
+    };
+
+    getAddOns();
+  }, []);
+  return (
+    <div>
+      <h1 className="menu-title">Step 2: Choose Your Add-Ons (Max 2)</h1>
+      <button className="home-button" onClick={returnHome}>
+        <img src={HomeButton} alt="home" />
+      </button>
+      <button className="navigationToMenu" onClick={navigateToMenu}>
+        <img src={arrow} alr="arrow" />
+      </button>
+      <div className="addon-container">
+        {addOns.map((addon, index) => (
+          <div
+            className="addon-entry"
+            key={index}
+            onMouseOver={(e) => handleHover(e, isHoverEnabled)}
+            onMouseOut={handleMouseOut}
+          >
+            <img
+              class="addon-square"
+              src={getImage(addon.name)}
+              alt={capitalizeName(addon.name, " ")}
+              onError={(e) => {
+                e.target.src = "/addOn_images/placeholder.png";
+              }}
+            />
+            <div className="addon-name">{capitalizeName(addon.name, " ")}</div>
+          </div>
+        ))}
+      </div>
+      <TextToSpeech isHoverEnabled={isHoverEnabled} toggleHover={toggleHover} />
+    </div>
+  );
+};
+
+export default MenuAddons;
