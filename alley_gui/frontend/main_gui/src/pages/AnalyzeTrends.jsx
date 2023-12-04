590cf2fc600563748bffdbc13866609a9fd3e6b9
Maya Shah <mayashah.tam@tamu.edu>
2023-12-04 00:01:13 -0600

    added high contrast temp button for analyze trends

--- alley_gui/frontend/main_gui/src/pages/AnalyzeTrends.jsx
+++ alley_gui/frontend/main_gui/src/pages/AnalyzeTrends.jsx
@@ -97,8 +97,36 @@ const AnalyzeTrends = () => {
       return formattedDate;
     };
 
+    const highContrastMode = () => {
+      const body = document.querySelector('body');
+      if (body.classList.contains("contrast")) {
+        body.classList.remove("contrast");
+        document.body.style.backgroundColor = '#ffefe2';
+        sessionStorage.setItem("high_contrast_mode", false);
+      } else {
+        body.classList.add("contrast");
+        document.body.style.backgroundColor = 'black';
+        sessionStorage.setItem("high_contrast_mode", true);
+      }
+    }
+
+    const loadCurrentMode = () => {
+      if (sessionStorage.getItem("high_contrast_mode")) {
+        const body = document.querySelector('body');
+        if (body.classList.contains("contrast") == false) {
+          body.classList.add("contrast");
+          document.body.style.backgroundColor = 'black';
+        }
+      } else {
+        const body = document.querySelector('body');
+        body.classList.remove("contrast");
+        document.body.style.backgroundColor = '#ffefe2';
+      }
+    }
+
     return (
-      <div>
+      <div onLoad={() => loadCurrentMode()}>
+        <button onClick={highContrastMode}>test</button>
         <h1 className="trendsTitle">Analyze Trends</h1>
         <button className="home-button" onClick={returnHome}> 
             <img src={HomeButton} alt="home" />
