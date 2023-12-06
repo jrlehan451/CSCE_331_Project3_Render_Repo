5789561862178b23f0ef51a580de3e9b138d9f34
Maya Shah <mayashah.tam@tamu.edu>
2023-12-05 22:59:14 -0600

    added server-side documentation

---
+++ webapp/swaggerDefinition.js
@@ -0,0 +1,8 @@
+module.exports = {
+    info: {
+      title: 'The Alley APIs',
+      version: '1.0.0',
+      description: 'all the server-side APIs used for The Alley POS Web Application',
+    },
+    basePath: '/',
+  };
